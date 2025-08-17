c7992717a18b7c45f3f1062ed7db5b7c83e6873a
Chandu-bodela <chandhubodela@gmail.com>
2025-08-17 14:48:09 +0530

    Create LC-3 Longest Substring Without Repeating Characters.java

---
+++ LC-3 Longest Substring Without Repeating Characters.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        int[] index=new int[128];
+        int maxLen=0;
+        int start=0;
+        for(int end=0;end<s.length();end++){
+            char c=s.charAt(end);
+            start=Math.max(index[c],start);
+            maxLen=Math.max(maxLen,end-start+1);
+            index[c]=end+1;
+        }
+        return maxLen;
+        
+    }
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+String s=sc.nextLine();
+Solution sol=new Solution();
+int res=sol.lengthOfLongestSubstring(s);
+System.out.println(res);
+}
+}
